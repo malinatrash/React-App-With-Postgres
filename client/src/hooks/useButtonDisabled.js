9d3f8e43f565dd061afeac6ba6dd7a57a92a85a0
Pavel <malinatrashh@gmail.com>
2023-04-29 20:57:24 +0800

    ура я делаю хук

---
+++ client/src/hooks/useButtonDisabled.js
@@ -0,0 +1,8 @@
+const {useState} = require("react");
+const useButtonDisabled = (name, lastname, phone) => {
+    const [fistName, setFirstName] = useState(name)
+    const [lastName, setLastName] = useState(lastname)
+    const [phoneNumber, setPhone] = useState(phone)
+
+
+}
