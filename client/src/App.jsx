05f300742bdd621b0bc6ad9d397ca44d4483ae3b
Pavel <malinatrashh@gmail.com>
2023-06-04 23:07:31 +0800

    fixx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,17 @@
-import AuthPage from "./pages/auth/AuthPage";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import SignUpForm from "./pages/auth/modules/SignUpForm";
-import SignInForm from "./pages/auth/modules/SignInForm";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import AuthPage from './pages/auth/AuthPage'
+import SignInForm from './pages/auth/modules/SignInForm'
+import SignUpForm from './pages/auth/modules/SignUpForm'
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    element={<AuthPage child={<SignUpForm/>}/>}
-                    path={'/signup'}
-                />
-                <Route
-                    element={<AuthPage child={<SignInForm/>}/>}
-                    path={'/signin'}
-                />
-                <Route
-                    path={'/*'}
-                    element={<Navigate to={'/signup'}/>}
-                />
-            </Routes>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<AuthPage child={<SignUpForm />} />} path={'/signup'} />
+				<Route element={<AuthPage child={<SignInForm />} />} path={'/signin'} />
+				<Route path={'/*'} element={<Navigate to={'/signup'} />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 export default App
